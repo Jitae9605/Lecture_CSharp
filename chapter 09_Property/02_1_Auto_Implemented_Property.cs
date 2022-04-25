73ddcca88c9e0b0d3c6e0092ccb415690297d914
Jitae9605 <dlwlxo3819@naver.com>
2022-04-25 11:43:15 +0900

    폴더이름교체 및 챕터9생성

---
+++ chapter 09_Property/02_1_Auto_Implemented_Property.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Auto_Implemented_Property
+{
+    // 자동구현 프로퍼티
+    class BirthdayInfo
+    {
+        public string Name { get; set; } = "Unknown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo();
+
+            // 생성만 했어도 초기값이 자동 삽입됨
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");   // BirthDay에서 값을 가져와서 계산하므로 값이 이상함.
+
+            // 값의 입력도 정상적으로 작동
+            birth.Name = "서현";
+            birth.Birthday = new DateTime(1991, 6, 28);
+            Console.WriteLine($"Name: {birth.Name}");
+            Console.WriteLine($"Birthday: {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age: {birth.Age}");
+        }
+    }
+}
