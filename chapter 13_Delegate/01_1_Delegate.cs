2558d54490ad4c519c8e364ff8958bf305c2ee43
Jitae9605 <dlwlxo3819@naver.com>
2022-04-28 14:25:57 +0900

    챕터 12 생성 및 예제 수행

---
+++ chapter 13_Delegate/01_1_Delegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Delegate
+{
+	// 대리자
+	// 메소드를 대신 호출해주는 호출자임.
+	class MainApp
+	{
+		delegate int MyDelegate(int a, int b);
+
+		class Calculator
+		{
+			public int Plus(int a, int b) { return a + b; }
+			public static int Minus(int a, int b) { return a - b; }
+		}
+		
+		static void Main(string[] args)
+		{
+			Calculator Calc = new Calculator();
+			MyDelegate Callback;
+
+			Callback = new MyDelegate(Calc.Plus);
+			Console.WriteLine(Callback(3, 4));
+
+			Callback = new MyDelegate(Calculator.Minus);
+			Console.WriteLine(Callback(7, 5));
+
+		}
+	}
+}
