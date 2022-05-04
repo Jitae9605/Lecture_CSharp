d945c0ee1484c581527a4e81f4890f01c1ea52e1
Jitae9605 <dlwlxo3819@naver.com>
2022-05-04 16:37:27 +0900

    챕터19 예제 추가

--- chapter 19_Thread_And_Task/04_1_Interupting_Thread.cs
+++ chapter 19_Thread_And_Task/04_1_Interupting_Thread.cs
@@ -4,11 +4,64 @@ using System.Threading;
 
 namespace Interupting_Thread
 {
+	class SideTask
+	{
+		int count;
+		public SideTask(int count)
+		{
+			this.count = count;
+		}
+
+		public void KeepAlive()
+		{
+			try
+			{
+				Console.WriteLine("Runing thread isn't gonna be interrupted");
+				Thread.SpinWait(1000000000);
+
+				while (count > 0)
+				{
+					Console.WriteLine($"{count--} left");
+
+					Console.WriteLine("Entering into WaitJoinSleep State...");
+					Thread.Sleep(10);
+				}
+				Console.WriteLine("Count : 0");
+
+			}
+
+			catch (ThreadInterruptedException e)
+			{
+				Console.WriteLine(e);
+			}
+
+			finally
+			{
+				Console.WriteLine("Clearing resource...");
+			}
+		}
+
+	}
 	class MainApp
 	{
 		static void Main(string[] args)
 		{
+			SideTask task = new SideTask(100);
+			Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+			t1.IsBackground = false;
 
+			Console.WriteLine("Starting thread...");
+			t1.Start();
+
+			Thread.Sleep(100);
+
+			Console.WriteLine("Interrupting thread...");
+			t1.Interrupt();                                 // 쓰레드 취소됨(catch문 실행)
+
+			Console.WriteLine("Waiting until thread stops...");
+			t1.Join();
+
+			Console.WriteLine("Finished...");
 		}
 	}
 }
