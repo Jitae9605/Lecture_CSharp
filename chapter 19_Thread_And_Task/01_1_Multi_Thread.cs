cee378e99ceb5b8399eb1a0428b0b249170eb400
Jitae9605 <dlwlxo3819@naver.com>
2022-05-04 12:27:40 +0900

    챕터19추가 및 정리 예제수행, 주석정리 완료

---
+++ chapter 19_Thread_And_Task/01_1_Multi_Thread.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Multi_Thread
+{
+	class MainApp
+	{
+		static void DoSomething()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine($"DoSomething : {i}");
+				Thread.Sleep(10);			// sleep = 10밀리초 동안 CPU사용 멈춤
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+			Console.WriteLine("Starting Thread...");
+			t1.Start();
+
+			for (int i = 0; i < 5; i++)
+			{
+				Console.WriteLine($"Main : {i}");
+				Thread.Sleep(10);
+			}
+
+			Console.WriteLine("wating until thread stop...");
+			t1.Join();
+
+			Console.WriteLine("Finished");
+
+		}
+	}
+}
---
+++ chapter 19_Thread_And_Task/04_1_Interupting_Thread.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace Interupting_Thread
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
