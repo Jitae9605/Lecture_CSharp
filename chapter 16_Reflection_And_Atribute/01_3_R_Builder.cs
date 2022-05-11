fc5de447bfc2358bf378771cd7de33656a8b4baa
Jitae9605 <dlwlxo3819@naver.com>
2022-05-11 16:51:45 +0900

    챕터 16 정리 및 주서작성 완료

---
+++ chapter 16_Reflection_And_Atribute/01_3_R_Builder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;	// 이게 있어야함
+
+namespace R_Builder
+{
+	// Builder를 이용하여 새로운 형식을 만들고 사용해보자
+	// 사용요령 :
+	// 1. AsssemblyBuilder로 어셈블리만든다
+	// 2. ModuleBuilder로 1.의 어셈블리안에 모듈을 생성해 삽입
+	// 3. 2.의 모듈안에 TypeBuilder로 클래스(형식)을 만들어 삽입
+	// 4. 3.의 클래스안에 MethodBuilder로 만든 메소드나 PropertyBuilder로 만든 프로퍼티 삽입
+	// 5. 4.에서 만든게 메소드라면, ILGenerator로 메소드안에 CPU가 실행할 IL명령들을 삽입 
+	class MainApp
+	{
+		static void Main()
+		{
+			// 1. AsssemblyBuilder로 어셈블리만든다
+			AssemblyBuilder newAssembly =
+				AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("CalculatorAssembly"), AssemblyBuilderAccess.Run);
+
+			// 2. ModuleBuilder로 1.의 어셈블리안에 모듈을 생성해 삽입
+			ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator");
+
+			// 3. 2.의 모듈안에 TypeBuilder로 클래스(형식)을 만들어 삽입
+			TypeBuilder newType = newModule.DefineType("Sum1To100");
+
+			// 4. 3.의 클래스안에 MethodBuilder로 만든 메소드나 PropertyBuilder로 만든 프로퍼티 삽입
+			MethodBuilder newMethod = newType.DefineMethod("Calculate", MethodAttributes.Public, typeof(int), new Type[0]);
+			//																					  반환형식		매개변수
+
+			// 5. 4.에서 만든게 메소드라면, ILGenerator로 메소드안에 CPU가 실행할 IL명령들을 삽입 
+			ILGenerator generator = newMethod.GetILGenerator();
+
+			generator.Emit(OpCodes.Ldc_I4, 1);	// 32비트 정수 1을 계산 스택에 넣는다.
+
+			for(int i = 2; i <= 100; i++)
+			{
+				generator.Emit(OpCodes.Ldc_I4, i);	// 32비트 정수 i를 계산스택에 넣는다.
+				generator.Emit(OpCodes.Add);		// 계산스택에 담겨있는 두 값을 더 계산한다음 그 결과값을 다시 계산스택에 넣는다.
+			}
+
+			generator.Emit(OpCodes.Ret);			// 계산스택에 담겨있는 값을 반환
+			newType.CreateType();
+
+			object sum1To100 = Activator.CreateInstance(newType);
+			MethodInfo Calculate = sum1To100.GetType().GetMethod("Calculate");
+			Console.WriteLine(Calculate.Invoke(sum1To100, null));
+
+		}
+	}
+}
---
+++ chapter 16_Reflection_And_Atribute/02_3_A_HistoryAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace A_HistoryAttribute
+{
+	// 나만의 커스텀 애트리뷰트를 만들고 정의해보자
+	//  - 애트리뷰트를 이용하여 버전관리를 해보자
+	[System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]	// 애트리뷰트의 애트리뷰트
+	class history : System.Attribute	// 이렇게 클래스에 상속만 시켜주면 이 클래스는 이제 애트리뷰트가 됨
+	{
+		private string programmer;
+		public double version;
+		public string changes;
+
+		public history(string programmer)
+		{
+			this.programmer = programmer;
+			version = 1.0;
+			changes = "First release";
+		}
+
+		public string GetProgrammer()
+		{
+			return programmer;
+		}
+	}
+
+	// 애트리뷰트 생성
+	[history("Sen", version = 0.1, changes = "2017-11-01 Created class stub")]
+	[history("Bob", version = 0.2, changes = "2020-12-03 Added Func() Method")]
+
+	class MyClass
+	{
+		public void Func()
+		{
+			Console.WriteLine("Func()");
+		}
+	}
+
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			Type type = typeof(MyClass);
+			Attribute[] attributes = Attribute.GetCustomAttributes(type);
+
+			Console.WriteLine("MyClass change history...");
+
+			foreach(Attribute a in attributes)
+			{
+				history h = a as history;
+				if (h != null)
+					Console.WriteLine("Ver: {0}, Programmer: {1}, changes: {2}", h.version, h.GetProgrammer(), h.changes);
+			}
+		}
+	}
+}
