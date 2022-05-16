43bed138a07bf18fdd3a26743ebab529d387ad95
Jitae9605 <dlwlxo3819@naver.com>
2022-05-16 13:41:21 +0900

    DB연결 추가2

--- DB연결툴_ADO.Net/ADO_Net_DB_Connet/Form1.Designer.cs
+++ DB연결툴_ADO.Net/ADO_Net_DB_Connet/Form1.Designer.cs
@@ -32,22 +32,34 @@ namespace ADO_Net_DB_Connet
 			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.btnConnect = new System.Windows.Forms.Button();
 			this.btnClose = new System.Windows.Forms.Button();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.btnSelect = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnDelete = new System.Windows.Forms.Button();
+			this.btnSum = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textName = new System.Windows.Forms.TextBox();
+			this.textAge = new System.Windows.Forms.TextBox();
+			this.btnInsert1 = new System.Windows.Forms.Button();
+			this.checkMale = new System.Windows.Forms.CheckBox();
+			this.btnInsert2 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// listBox1
 			// 
 			this.listBox1.FormattingEnabled = true;
 			this.listBox1.ItemHeight = 12;
-			this.listBox1.Location = new System.Drawing.Point(23, 18);
+			this.listBox1.Location = new System.Drawing.Point(14, 12);
 			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(380, 172);
+			this.listBox1.Size = new System.Drawing.Size(192, 172);
 			this.listBox1.TabIndex = 0;
 			// 
 			// btnConnect
 			// 
-			this.btnConnect.Location = new System.Drawing.Point(23, 210);
+			this.btnConnect.Location = new System.Drawing.Point(14, 190);
 			this.btnConnect.Name = "btnConnect";
-			this.btnConnect.Size = new System.Drawing.Size(192, 43);
+			this.btnConnect.Size = new System.Drawing.Size(92, 38);
 			this.btnConnect.TabIndex = 1;
 			this.btnConnect.Text = "연결";
 			this.btnConnect.UseVisualStyleBackColor = true;
@@ -55,26 +67,150 @@ namespace ADO_Net_DB_Connet
 			// 
 			// btnClose
 			// 
-			this.btnClose.Location = new System.Drawing.Point(221, 210);
+			this.btnClose.Location = new System.Drawing.Point(112, 190);
 			this.btnClose.Name = "btnClose";
-			this.btnClose.Size = new System.Drawing.Size(182, 43);
+			this.btnClose.Size = new System.Drawing.Size(94, 38);
 			this.btnClose.TabIndex = 1;
 			this.btnClose.Text = "연결해제";
 			this.btnClose.UseVisualStyleBackColor = true;
 			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
 			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 12;
+			this.listBox2.Location = new System.Drawing.Point(225, 12);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(308, 172);
+			this.listBox2.TabIndex = 0;
+			// 
+			// btnSelect
+			// 
+			this.btnSelect.Location = new System.Drawing.Point(539, 12);
+			this.btnSelect.Name = "btnSelect";
+			this.btnSelect.Size = new System.Drawing.Size(61, 38);
+			this.btnSelect.TabIndex = 2;
+			this.btnSelect.Text = "select";
+			this.btnSelect.UseVisualStyleBackColor = true;
+			this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.Location = new System.Drawing.Point(539, 56);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(61, 38);
+			this.btnUpdate.TabIndex = 2;
+			this.btnUpdate.Text = "update";
+			this.btnUpdate.UseVisualStyleBackColor = true;
+			this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+			// 
+			// btnDelete
+			// 
+			this.btnDelete.Location = new System.Drawing.Point(539, 100);
+			this.btnDelete.Name = "btnDelete";
+			this.btnDelete.Size = new System.Drawing.Size(61, 38);
+			this.btnDelete.TabIndex = 2;
+			this.btnDelete.Text = "delete";
+			this.btnDelete.UseVisualStyleBackColor = true;
+			this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+			// 
+			// btnSum
+			// 
+			this.btnSum.Location = new System.Drawing.Point(539, 146);
+			this.btnSum.Name = "btnSum";
+			this.btnSum.Size = new System.Drawing.Size(61, 38);
+			this.btnSum.TabIndex = 2;
+			this.btnSum.Text = "sum";
+			this.btnSum.UseVisualStyleBackColor = true;
+			this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(223, 203);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(37, 12);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "이름 :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(345, 203);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(37, 12);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "나이 :";
+			// 
+			// textName
+			// 
+			this.textName.Location = new System.Drawing.Point(266, 200);
+			this.textName.Name = "textName";
+			this.textName.Size = new System.Drawing.Size(73, 21);
+			this.textName.TabIndex = 5;
+			// 
+			// textAge
+			// 
+			this.textAge.Location = new System.Drawing.Point(388, 200);
+			this.textAge.Name = "textAge";
+			this.textAge.Size = new System.Drawing.Size(80, 21);
+			this.textAge.TabIndex = 5;
+			// 
+			// btnInsert1
+			// 
+			this.btnInsert1.Location = new System.Drawing.Point(225, 227);
+			this.btnInsert1.Name = "btnInsert1";
+			this.btnInsert1.Size = new System.Drawing.Size(114, 29);
+			this.btnInsert1.TabIndex = 6;
+			this.btnInsert1.Text = "Insert 1";
+			this.btnInsert1.UseVisualStyleBackColor = true;
+			this.btnInsert1.Click += new System.EventHandler(this.btnInsert1_Click);
+			// 
+			// checkMale
+			// 
+			this.checkMale.AutoSize = true;
+			this.checkMale.Location = new System.Drawing.Point(485, 202);
+			this.checkMale.Name = "checkMale";
+			this.checkMale.Size = new System.Drawing.Size(48, 16);
+			this.checkMale.TabIndex = 8;
+			this.checkMale.Text = "남자";
+			this.checkMale.UseVisualStyleBackColor = true;
+			// 
+			// btnInsert2
+			// 
+			this.btnInsert2.Location = new System.Drawing.Point(354, 227);
+			this.btnInsert2.Name = "btnInsert2";
+			this.btnInsert2.Size = new System.Drawing.Size(114, 29);
+			this.btnInsert2.TabIndex = 6;
+			this.btnInsert2.Text = "Insert 2";
+			this.btnInsert2.UseVisualStyleBackColor = true;
+			this.btnInsert2.Click += new System.EventHandler(this.btnInsert2_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(437, 266);
+			this.ClientSize = new System.Drawing.Size(676, 302);
+			this.Controls.Add(this.checkMale);
+			this.Controls.Add(this.btnInsert2);
+			this.Controls.Add(this.btnInsert1);
+			this.Controls.Add(this.textAge);
+			this.Controls.Add(this.textName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnSum);
+			this.Controls.Add(this.btnDelete);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnSelect);
 			this.Controls.Add(this.btnClose);
 			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.listBox2);
 			this.Controls.Add(this.listBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -83,6 +219,18 @@ namespace ADO_Net_DB_Connet
 		private System.Windows.Forms.ListBox listBox1;
 		private System.Windows.Forms.Button btnConnect;
 		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Button btnSelect;
+		private System.Windows.Forms.Button btnUpdate;
+		private System.Windows.Forms.Button btnDelete;
+		private System.Windows.Forms.Button btnSum;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textName;
+		private System.Windows.Forms.TextBox textAge;
+		private System.Windows.Forms.Button btnInsert1;
+		private System.Windows.Forms.CheckBox checkMale;
+		private System.Windows.Forms.Button btnInsert2;
 	}
 }
 
