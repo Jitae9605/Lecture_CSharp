3b01b86826560871a1b762c6c8676ae14aa76438
Jitae9605 <dlwlxo3819@naver.com>
2022-06-14 10:42:29 +0900

    220614 실글톤 패턴 학습

---
+++ assignment/ADO_Net/mook_CarInfo - 주석 없는 버전/AdoNetDB/IDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDB
+{
+	public interface IDatabase
+	{
+		string ConnectionString { get; set; }
+
+		// 여러가지 DB(Oracle, MsSql, MySql) 등을 묶어주는 역할을 함. 공통적인 정보를 가져와 쓸 수 있음.
+		IDbConnection Connection { get; set; }
+	}
+}
---
+++ assignment/ADO_Net/mook_CarInfo - 주석 없는 버전/ConfigurationMgr/ConfigurationMgr.cs
@@ -0,0 +1,63 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConfigurationMgr
+{
+	public class ConfigurationMgr
+	{
+		private static ConfigurationMgr instance;
+
+
+		public string ConnectionString { get; set; }
+
+		IDbConnection connetion;
+
+		public ConfigurationMgr()
+		{
+			// App.config 파일을 읽어서 로딩
+			LoadConfiguration();
+
+
+			// connection 생성
+			MakeConnetion();
+		}
+
+		private void LoadConfiguration()
+		{
+			// App.config의 내용을 전부 가져와서 config에 저장하는 문장
+			Configuration config = 
+				ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+			ConnectionString = 
+				config.ConnectionStrings.ConnectionStrings["WinConnection"].ConnectionString;
+		}
+
+		public IDbConnection Connection
+		{
+			get
+			{
+				if (connetion == null)
+					connetion = new SqlConnection(ConnectionString);	// 간단하게 만든 싱글톤 패턴
+
+				// SqlConnetion 생성과 동시에 connetion open 처리를 추가해도 됨
+				// connectio.open()
+				return Connection;
+			}
+			private set { }
+		}
+
+		private void MakeConnetion()
+		{
+			connetion = new SqlConnection(ConnectionString);
+		}
+
+		public static ConfigurationMgr Instace()
+		{
+			if (instance == null)
+				instance = new ConfigurationMgr();	// ConfigurationMgr 의 싱글톤
+			return instance;
+		}
+	}
+
+}
